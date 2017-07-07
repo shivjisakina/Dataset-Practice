9cef3fc083b9041da28baf87c904ab9317fcff2f
Sakina Shivji <shivjisakina@gmail.com>
2017-07-06 20:45:12 -0400

    added update button

--- controllers/controller.js
+++ controllers/controller.js
@@ -34,7 +34,17 @@ module.exports = function (app) {
     });
 
     app.delete("/:City", function(req, res) {
-        connection.query("DELETE FROM quotes WHERE City = ?", [req.params.city], function(err, result) {
+        connection.query("DELETE FROM quotes WHERE City = ?", [req.params.City], function(err, result) {
+            if (err) {
+                throw err;
+            }
+            res.redirect("/");
+        });
+    });
+
+    app.put("/:City", function(req, res) {
+        connection.query("UPDATE cities SET City = ?, City = ? WHERE City = ?", [
+            req.body.cityu, req.body.countryu, req.params.city], function(err, result) {
             if (err) {
                 throw err;
             }
